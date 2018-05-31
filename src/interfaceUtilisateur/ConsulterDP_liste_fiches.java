573f22a17e04be71ab5d7e91c128a57f61d3ce92
manoncaulier <caulier.manon18@orange.fr>
2018-05-31 11:42:50 +0200

    modif manon

--- src/interfaceUtilisateur/ConsulterDP_liste_fiches.java
+++ src/interfaceUtilisateur/ConsulterDP_liste_fiches.java
@@ -9,12 +9,12 @@ package interfaceUtilisateur;
  *
  * @author annelise
  */
-public class ConsulterDP extends javax.swing.JFrame {
+public class ConsulterDP_liste_fiches extends javax.swing.JFrame {
 
     /**
      * Creates new form ConsulterDP
      */
-    public ConsulterDP() {
+    public ConsulterDP_liste_fiches() {
         initComponents();
     }
 
@@ -429,20 +429,21 @@ public class ConsulterDP extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ConsulterDP.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ConsulterDP_liste_fiches.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ConsulterDP.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ConsulterDP_liste_fiches.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ConsulterDP.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ConsulterDP_liste_fiches.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ConsulterDP.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ConsulterDP_liste_fiches.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ConsulterDP().setVisible(true);
+                new ConsulterDP_liste_fiches().setVisible(true);
             }
         });
     }
