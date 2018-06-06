d4ad552b079884cce82db66f148a9a263b2102d2
neilerua72 <neilerua72@gmail.com>
2018-06-06 15:35:43 +0200

    Ajout classe MedelTabl

---
+++ src/Listenner/ModelTable.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Listenner;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Aurélien
+ */
+public class ModelTable extends DefaultTableModel{
+    
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex){
+        return false;
+    }
+}
